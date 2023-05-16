0cc880248382511753631d908624224641190e4e
James Belcher <130000791+Tankb220@users.noreply.github.com>
2023-05-15 22:13:26 -0500

    finished rest of java script creating local storage

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -42,7 +42,7 @@ const questions = [
   },
   {
     questionText:
-      "Which park was establish in 2020 and is the deepest gorge east of the Mississippi River",
+      "Which park was established in 2020 and is the deepest gorge east of the Mississippi River",
     options: [
       "1. New River Gorge",
        "2. Great Smokey Mountains", 
@@ -126,4 +126,170 @@ function displayQuestion() {
     let optionButton = document.querySelector("#option" + i);
     optionButton.textContent = option;
   }
+}
+//behaviour when an answer button is clicked: click  with id "quiz-options"//
+//eventObject.target identifies the specific  element that was clicked on//
+document.querySelector("#quiz-options").addEventListener("click", checkAnswer);
+
+
+function optionIsCorrect(optionButton) {
+  return optionButton.textContent === questions[currentQuestion].answer;
+}
+
+//if answer is incorrect, penalise time//
+function checkAnswer(eventObject) {
+  let optionButton = eventObject.target;
+  resultDiv.style.display = "block";
+  if (optionIsCorrect(optionButton)) {
+    resultText.textContent = "Correct!";
+    setTimeout(hideResultText, 1000);
+  } else {
+    resultText.textContent = "Incorrect!";
+    setTimeout(hideResultText, 1000);
+    if (time >= 10) {
+      time = time - 10;
+      displayTime();
+    } else {
+      //if time is less than 10,  end quiz//
+    
+      time = 0;
+      displayTime();
+      endQuiz();
+    }
+  }
+
+  //increment current question by 1//
+  currentQuestion++;
+  //if we have not run out of questions then display next question, else end quiz//
+  if (currentQuestion < questions.length) {
+    displayQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+//display scorecard and hide other divs//
+const score = document.querySelector("#score");
+
+//at end of quiz, clear the timer, hide any visible cards and display the scorecard //
+function endQuiz() {
+  clearInterval(intervalID);
+  hideCards();
+  scoreCard.removeAttribute("hidden");
+  score.textContent = time;
+}
+
+const submitButton = document.querySelector("#submit");
+const inputElement = document.querySelector("#initials");
+
+//store user initials and score when submit button is clicked//
+submitButton.addEventListener("click", storeScore);
+
+function storeScore(event) {
+
+  event.preventDefault();
+
+  //check for input//
+  if (!inputElement.value) {
+    alert("Enter Initials before submitting");
+    return;
+  }
+
+ 
+  let leaderboardItem = {
+    initials: inputElement.value,
+    score: time,
+  };
+
+  updateStoredLeaderboard(leaderboardItem);
+
+ 
+  hideCards();
+  leaderboardCard.removeAttribute("hidden");
+
+  renderLeaderboard();
+}
+
+//updates the leaderboard//
+function updateStoredLeaderboard(leaderboardItem) {
+  let leaderboardArray = getLeaderboard();
+  //update leaderboard item to leaderboard array//
+  leaderboardArray.push(leaderboardItem);
+  localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
+}
+
+//get "leaderboardArray" from local storage (if it exists) and parse it into a javascript object ///
+function getLeaderboard() {
+  let storedLeaderboard = localStorage.getItem("leaderboardArray");
+  if (storedLeaderboard !== null) {
+    let leaderboardArray = JSON.parse(storedLeaderboard);
+    return leaderboardArray;
+  } else {
+    leaderboardArray = [];
+  }
+  return leaderboardArray;
+}
+
+//display leaderboard //
+function renderLeaderboard() {
+  let sortedLeaderboardArray = sortLeaderboard();
+  const highscoreList = document.querySelector("#highscore");
+  highscoreList.innerHTML = "";
+  for (let i = 0; i < sortedLeaderboardArray.length; i++) {
+    let leaderboardEntry = sortedLeaderboardArray[i];
+    let newListItem = document.createElement("li");
+    newListItem.textContent =
+      leaderboardEntry.initials + " - " + leaderboardEntry.score;
+    highscoreList.append(newListItem);
+  }
+}
+
+//sort leaderboard array from highest to lowest//
+function sortLeaderboard() {
+  let leaderboardArray = getLeaderboard();
+  if (!leaderboardArray) {
+    return;
+  }
+
+  leaderboardArray.sort(function (a, b) {
+    return b.score - a.score;
+  });
+  return leaderboardArray;
+}
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", clearHighscores);
+
+//clear local storage and display empty leaderboard//
+function clearHighscores() {
+  localStorage.clear();
+  renderLeaderboard();
+}
+
+const backButton = document.querySelector("#back");
+backButton.addEventListener("click", returnToStart);
+
+
+function returnToStart() {
+  hideCards();
+  startCard.removeAttribute("hidden");
+}
+
+//use link to view highscores from any point on the page//
+const leaderboardLink = document.querySelector("#leaderboard-link");
+leaderboardLink.addEventListener("click", showLeaderboard);
+
+function showLeaderboard() {
+  hideCards();
+  leaderboardCard.removeAttribute("hidden");
+
+  //stop countdown//
+  clearInterval(intervalID);
+
+  //assign undefined to time and display that, so that time does not appear on page//
+  time = undefined;
+  displayTime();
+
+  //display leaderboard on leaderboard card//
+  renderLeaderboard();
 }
